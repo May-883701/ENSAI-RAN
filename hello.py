1fb7f58e4725e9edbeaafecfc60dcef70d7123b8
id2931-ensai <isabelle.kantoussan@eleve.ensai.fr>
2025-09-10 12:02:28 +0000

    resolve conflict

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-print('hello')
-=======
-<<<<<<< HEAD
 print('Hello')
 print('How are you?')
-=======
 print("Hello c'est olive ")
->>>>>>> 744bb0029a66a4ccec873b7eb3d5e9bb1d0f7dae
->>>>>>> 7a79bb23add9aeebe2b88a258092ee7262f600f3
